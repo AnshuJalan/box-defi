bbd784ca247e96d07fc0f239c4608583af963326
AnshuJalan <anshujalan206@gmail.com>
2021-12-01 11:34:08 +0530

    chore: add tzip-21 metadata builder

---
+++ deploy/src/utils/metadata_builder.ts
@@ -0,0 +1,41 @@
+import { create } from "ipfs-http-client";
+
+const ipfs = create({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
+
+const ARTIFACTS = [
+  {
+    uri: "ipfs://QmW3a6DjufEr39HkyGpC3aECK3QP5CuvEZdG6578Fq4xLd",
+    name: "BoxFruit #1 - Elder Grape",
+  },
+  {
+    uri: "ipfs://QmU1fpW53pThaBmTzgxeUntDnwen4MUS7NXqZGNDzTUQDJ",
+    name: "BoxFruit #2 - Mangrot",
+  },
+  {
+    uri: "ipfs://QmUdzpwNXZpMnjh58D3ois4miyp22zaJUdTpJ4hVs5ZAyE",
+    name: "BoxFruit #3 - Spot Berry",
+  },
+  {
+    uri: "ipfs://Qmd6e1XVnM31PuxS7PYe7cBY6sRFKiv2mnzuRbzLNbCZo3",
+    name: "BoxFruit #4 - Blue Stripe",
+  },
+  {
+    uri: "ipfs://QmQqyE8nfZuhSx4ww279VrMXrXt4PeYt4PuG9ofpWfMbqE",
+    name: "BoxFruit #5 - Crown Apple",
+  },
+];
+
+(async () => {
+  for (const artifact of ARTIFACTS) {
+    const metadata = {
+      artifactUri: artifact.uri,
+      displayUri: artifact.uri,
+      thumbnailUri: artifact.uri,
+      decimals: 0,
+      symbol: "FRUIT",
+      name: artifact.name,
+    };
+    const { cid } = await ipfs.add(JSON.stringify(metadata));
+    console.log(Buffer.from(`ipfs://${cid.toString()}`).toString("hex"));
+  }
+})();
