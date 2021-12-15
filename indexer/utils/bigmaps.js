6254ade27c23c14e5caf303b444196c618798fc0
AnshuJalan <anshujalan206@gmail.com>
2021-12-15 19:07:45 +0530

    feat: add action wise key updation

--- indexer/utils/bigmaps.js
+++ indexer/utils/bigmaps.js
@@ -5,34 +5,37 @@ const global = require("./global");
 const boxModel = require("../models/box");
 
 const updateKey = async (item) => {
-  const box = await boxModel.findOne({ key: item.key });
-
-  if (box) {
-    box.lastWatered = item.value.last_watered;
-    box.timesWatered = item.value.times_watered;
-    await box.save();
+  if (item.action !== "remove_key") {
+    const box = await boxModel.findOne({ key: item.content.key });
+    if (box) {
+      box.lastWatered = item.content.value.last_watered;
+      box.timesWatered = item.content.value.times_watered;
+      await box.save();
+    } else {
+      const newBox = new boxModel({
+        key: item.content.key,
+        creator: item.content.value.creator,
+        lastWatered: item.content.value.last_watered,
+        timesWatered: item.content.value.times_watered,
+      });
+      await newBox.save();
+    }
   } else {
-    const newBox = new boxModel({
-      key: item.key,
-      creator: item.value.creator,
-      lastWatered: item.value.last_watered,
-      timesWatered: item.value.times_watered,
-    });
-    await newBox.save();
+    await boxModel.findOneAndRemove({ key: item.content.key });
   }
 };
 
 const syncBigMap = async () => {
-  const _res = await axios.get(`${global.tzktAPI}/bigmaps/${global.boxesBigMap}/keys`);
-  for (const item of _res.data) {
-    updateKey(item);
+  const _res = await axios.get(`${global.tzktAPI}/bigmaps/updates?bigmap=${global.boxesBigMap}`);
+  for (const item of _res.data.slice(1)) {
+    await updateKey(item);
   }
 };
 
 const updateBigMap = (updates) => {
-  updates.data.forEach((item) => {
-    updateKey(item.content);
-  });
+  for (const item of updates.data) {
+    updateKey(item);
+  }
 };
 
 module.exports = {
--- indexer/utils/global.js
+++ indexer/utils/global.js
@@ -1,6 +1,6 @@
 module.exports = {
   tzktAPI: process.env.TZKT_API || "https://api.hangzhou2net.tzkt.io/v1",
-  boxesBigMap: process.env.BOXES_BIGMAP || 37259,
+  boxesBigMap: process.env.BOXES_BIGMAP || 37550,
   port: process.env.PORT || 3001,
   mongodbURL: process.env.MONGODB_URL || "mongodb://localhost:27017/box-defi",
 };
