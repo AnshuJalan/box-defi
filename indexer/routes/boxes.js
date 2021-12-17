b67071b52c5c6ae5ebf0bf088f4d5a63b3e129f4
AnshuJalan <anshujalan206@gmail.com>
2021-12-17 19:36:00 +0530

    feat: # boxes retrieval

--- indexer/routes/boxes.js
+++ indexer/routes/boxes.js
@@ -4,7 +4,8 @@ const boxModel = require("../models/box");
 router.get("/", async (req, res) => {
   const address = req.query.address;
   if (!address) {
-    res.sendStatus(400);
+    const numBoxes = await boxModel.countDocuments();
+    res.status(200).send({ numBoxes });
   } else {
     const boxes = await boxModel.find({ creator: address });
     res.status(200).send(boxes);
