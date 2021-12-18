22153b6585ef58d93fe88ef827dc5ec367f5ae1d
AnshuJalan <anshujalan206@gmail.com>
2021-12-18 19:38:11 +0530

    feat: reconnect on tzkt breakage

--- indexer/utils/global.js
+++ indexer/utils/global.js
@@ -1,6 +1,6 @@
 module.exports = {
   tzktAPI: process.env.TZKT_API || "https://api.hangzhou2net.tzkt.io/v1",
-  boxesBigMap: process.env.BOXES_BIGMAP || 37550,
+  boxesBigMap: process.env.BOXES_BIGMAP || 44300,
   port: process.env.PORT || 3001,
   mongodbURL: process.env.MONGODB_URL || "mongodb://localhost:27017/box-defi",
 };
--- indexer/utils/tzkt.js
+++ indexer/utils/tzkt.js
@@ -1,16 +1,29 @@
 const signalR = require("@microsoft/signalr");
 const global = require("./global");
+const BigMaps = require("./bigmaps");
 
 const connection = new signalR.HubConnectionBuilder().withUrl(`${global.tzktAPI}/events`).build();
 
 const init = async () => {
-  // open connection
-  await connection.start();
+  try {
+    // open connection
+    await connection.start();
 
-  // Subscribe to BigMap changes
-  await connection.invoke("SubscribeToBigMaps", {
-    ptr: global.boxesBigMap,
-  });
+    // Subscribe to BigMap changes
+    await connection.invoke("SubscribeToBigMaps", {
+      ptr: global.boxesBigMap,
+    });
+  } catch (err) {
+    // Attempt reconnection
+    setTimeout(attemptReconnection, 10000);
+  }
+};
+
+// TODO: make this more efficient
+const attemptReconnection = async () => {
+  if (connection.state !== "Connected") await init();
+  // Sync bigMaps
+  await BigMaps.syncBigMap();
 };
 
 module.exports = {
